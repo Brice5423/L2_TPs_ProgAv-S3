8d172425c470ddc9ef037542be14b166cf2cb731
Brice Briçou <briceorl54580@gmail.com>
2021-10-06 12:01:48 +0200

    Update : intlist.c. Récupération de la nouvelle version sur Arche...

--- TP3_ProgAv-S3/intlist.c
+++ TP3_ProgAv-S3/intlist.c
@@ -1,29 +1,24 @@
 /* ****** ****** ****** ****** ****** Les directives de compilation ****** ****** ****** ****** ****** */
 
 /**
-	* @note
-	*  Compiler avec
-	*    gcc -Wall -std=c11 -o intlist intlist.c
-	*
-	* @brief
-	*  Conception d'un type pour les listes d'entiers
-	*  et des focntions associées
-	*
-	* @TODO
-	*  Ajoutez les fonctions de consultation des éléments de liste :
-	*  + getX
-	*  + getSuc
-	*  + setX
-	*  + setSuc
-	*
-	* @TODO
-	*  Puis, séparez les éléments afin qu'apparaissent :
-	*  + Un fichier d'entêtes (.h) de la bibliothèque liste d'entiers,
-	*  + Un fichier de définitions (.c) correspondant aux déclarations du fichier d'entête
-	*  + Un fichier main.c ne contenant que la fonction principale.
-	*
-	* @TODO
-	*	Construisez le Makefile correspondant
+	@note
+		Compiler avec : gcc -Wall -std=c11 -o intlist intlist.c
+	@brief
+		Conception d'un type pour les listes d'entiers et des fonctions associées
+	@TODO
+		Ajoutez les fonctions de consultation des éléments de liste :
+		+ new_lst_elm
+		+ del_lst_elm
+		+ getX
+		+ getSuc
+		+ setX
+		+ setSuc
+		Puis, séparez les éléments afin qu'apparaissent :
+		+ Un fichier d'entêtes (.h) de la bibliothèque élément de liste d'entiers,
+		+ Un fichier d'entêtes (.h) de la bibliothèque liste d'entiers,
+		+ Les fichiers de définitions (.c) correspondant aux déclarations du fichier d'entête
+		+ Un fichier main.c ne contenant que la fonction principale.
+		Enfin, construisez le Makefile
 */
 
 #include <stdlib.h> // librairie standard
@@ -34,65 +29,56 @@
 
 /* ****** ****** ****** ****** ****** Les déclarations des TA et des fonctions ****** ****** ****** ****** ****** */
 
-/***
- * LES TYPES ABSTRAITS
- ***/
+/* ***** ***** LES TYPES ABSTRAITS ***** ***** */
 
-/** @brief Le type d'un élément de liste:
- * + x - un entier,
- * + suc - un pointeur sur son successeur (ou NULL s'il n'y en a pas)
- */
+/** @brief Le type abstrait d'un _élément_ de liste:
+	+ x - un entier,
+	+ suc - un pointeur sur son successeur (ou NULL s'il n'y en a pas)
+*/
 struct lst_elm_t {
-  int x;
-  struct lst_elm_t * suc;
+    int x;
+    struct lst_elm_t * suc;
 };
 
-/***
- * DÉCLARATION DES FONCTIONS PUBLIQUES
- ***/
+/* ***** ***** DÉCLARATION DES FONCTIONS ASSOCIÉES ***** ***** */
 
-/** @brief Création d'un nouvel élément de liste d'entiers */
+/** @brief Créer un élément de liste et y ranger la valeur entière value */
 struct lst_elm_t * new_lst_elm(int value);
 
-/** @brief Suppression d'un élément de liste d'entiers */
-void del_lst_elm_t(struct lst_elm_t **ptrE);
+/** @brief Supprimer un élément de liste et mettre son pointeur à NULL */
+void del_lst_elm(struct lst_elm_t ** ptrE);
 
-/** @brief Accéder au champ x de l'élément de liste d'entiers */
-int getX(struct lst_elm_t *E);
+/** @brief Renvoyer la valeur entière de l'élément */
+int getX(struct lst_elm_t * E);
 
-/** @ brief Accéder au champ suc de l'élément de liste d'entiers */
-structlst_elm_t * getSuc(struct lst_elm_t *E);
+/** @brief Renvoyer le pointeur sur le successeur de l'élément */
+struct lst_elm_t * getSuc(struct lst_elm_t * E);
 
-/** @brief Modifier la valeur du champ x de l'élément de liste d'entiers */
-void setX(struct lst_elm_t *E, int v);
+/** @brief Modifier la valeur entière de l'élément */
+void setX(struct lst_elm_t * E, int v);
 
-/** @brief Modifier la valeur du champ suc de l'élément de liste d'entiers */
-void setSuc(struct lst_elm_t *E, struct lst_elm_t *S);
+/** @brief Modifier le pointeur sur le successeur de l'élément */
+void setSuc(struct lst_elm_t * E, struct lst_elm_t * S);
 
 
-/** @brief Le type d'une liste :
+/** @brief Le type abstrait d'une _liste_:
 	* + head - le premier élément de la liste
 	* + tail - le dernier élément de la liste
 	* + numelm - le nombre d'élément dans la liste
 */
 struct lst_t {
-	struct lst_elm_t * head;
-	struct lst_elm_t * tail;
-	int numelm;
+    struct lst_elm_t * head;
+    struct lst_elm_t * tail;
+    int numelm;
 };
 
-/***
- * DÉCLARATION DES FONCTIONS PUBLIQUES
- ***/
-
-/** @brief Construire une liste vide */
-struct lst_t * new_lst();
-
-/** @brief Libèrer la mémoire occupée par la liste */
-void del_lst(struct lst_t ** ptrL );
+/* ***** ***** DÉCLARATION DES FONCTIONS ASSOCIÉES ***** ***** */
 
 /** @brief Vérifier si la liste L est vide ou pas */
 bool empty_lst(const struct lst_t * L);
+
+/** @brief Construire une liste vide */
+struct lst_t * new_lst();
 
 /** @brief Ajouter en tête de la liste L la valeur v */
 void cons(struct lst_t * L, int v);
@@ -100,58 +86,54 @@ void cons(struct lst_t * L, int v);
 /** @brief Visualiser les éléments de la liste L */
 void print_lst(struct lst_t * L );
 
+/** @brief Libèrer la mémoire occupée par la liste */
+void del_lst(struct lst_t ** ptrL );
+
 
 /* ****** ****** ****** ****** ****** ****** La fonction principale main ****** ****** ****** ****** ****** ****** */
 
-/***
-	* ALGORITHME (FONCTION PRINCIPALE)
-	***/
+/* ***** ***** ALGORITHME (FONCTION PRINCIPALE) ***** ***** */
 
 int main() {
 	int v;
 	struct lst_t * L = new_lst();
-	scanf( "%d", &v );
+	scanf("%d", &v);
 	while( v > 0 ) {
-		cons(L,v);
-		scanf("%d",&v);
+		cons(L, v);
+		scanf("%d", &v);
 	}
 	print_lst(L);
-	del_lst(L);
+	del_lst(&L);
 	return EXIT_SUCCESS;
 }
 
 
 /* ****** ****** ****** ****** ****** ****** Les définitions des fonctions ****** ****** ****** ****** ****** ****** */
 
-/***
-  * DÉFINITIONS DES FONCTIONS
-  ***/
+/* ***** ***** DÉFINITIONS DES FONCTIONS
+	@TODO Créez les fonctions:
+	new_lst_elm, del_lst_elm, getX, getSuc, setX, setSuc
+	     x            x        v      v      v      v
+***** ***** */
 
-/** @todo Définissez les fonctions publiques :
- * + new_lst_elm
- * + del_lst_elm
- * + getX /
- * + getSuc /
- * + setX /
- * + setSuc /
- * des éléments de liste d'entiers.
- **/
+struct lst_t * new_lst_elm(int value) {
+	/** @note : calloc fonctionne de manière identique à malloc
+		et de surcroît met à NULL(0) tous les octets alloués */
+	struct lst_t * L = (struct lst_t *)calloc(1,sizeof(struct lst_t));
+	assert(L);
+	L->x = value
+	return L;
+}
 
- struct lst_elm_t * new_lst_elm(int value) { // ***********************************************************************
-    struct lst_elm_t L;
-    L.x = value;
-    return L;
- }
+void del_lst_elm(struct lst_elm_t ** ptrE) {
 
- void del_lst_elm_t(struct lst_elm_t **ptrE) { // ********************************************************************
-
- }
+}
 
 int getX(struct lst_elm_t *E) {
     return E->x;
 }
 
-struct lst_elm_t *getSuc(struct lst_elm_t *E) {
+struct lst_elm_t * getSuc(struct lst_elm_t *E) {
     return E->suc;
 }
 
@@ -163,33 +145,32 @@ void setSuc(struct lst_elm_t *E, struct lst_elm_t *S) {
     E->suc = S->suc;
 }
 
-struct lst_t * new_lst() {
-	/**
-	 * @note : calloc fonctionne de manière identique à malloc
-	 * et de surcroît met à NULL(0) tous les octets alloués
-	 **/
-	struct lst_t * L = (struct lst_t *)calloc(1, sizeof(struct lst_t));
-	assert(L);
-	return L;
-}
-
-void del_lst(struct lst_t ** ptrL ) {
-	/** @todo */
-}
 
 bool empty_lst(const struct lst_t * L ) {
-	assert(L);// L doit exister !
+	assert(L);
 	return L->numelm == 0;
 }
 
+struct lst_t * new_lst() {
+    /** @note : calloc fonctionne de manière identique à malloc
+    	et de surcroît met à NULL(0) tous les octets alloués */
+    struct lst_t * L = (struct lst_t *)calloc(1,sizeof(struct lst_t));
+    assert(L);
+    return L;
+}
+
 void cons(struct lst_t * L, int v) {
-	/** @todo */
+	/* @TODO */
 }
 
 void print_lst(struct lst_t * L ) {
 	printf( "[ " );
-	for( struct lst_elm_t * E = L->head; E; E = E->suc) {
+	for(struct lst_elm_t * E = L->head; E; E = E->suc) {
 		printf( "%d ", E->x );
 	}
 	printf( "]\n\n" );
+}
+
+void del_lst(struct lst_t ** ptrL ) { /* del_lst_elm_t */
+	/* @TODO */
 }
